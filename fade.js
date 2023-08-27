790fdaad098ae20d54bb4e166ce90406ac2a226e
Your Name <a.cooper8789@gmail.com>
2023-08-27 16:05:11 +0100

    Fixed title not appearing on top of paralax

--- fade.js
+++ fade.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+  
+    if (scroll >= 500) {
+      $(".menu").addClass("transparent");
+    } else {
+      $(".menu").removeClass("transparent");
+    }
+  });
+
+
+  
